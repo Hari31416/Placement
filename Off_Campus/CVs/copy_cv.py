4ba803f5c7b43f59b1cd36863c9f3ae17cd8b565
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2023-05-21 19:35:39 +0530

    Added some more CVs and reorganized them

--- Off_Campus/CVs/copy_cv.py
+++ Off_Campus/CVs/copy_cv.py
@@ -1,22 +1,45 @@
+#!/usr/bin/python3
 import os
 import glob
 import sys
 from style import cprint
 
+DIRS = [
+    "data_analyst",
+    "finance",
+    "ML_DL",
+]
+
+
+def search_dir(directory):
+    for dir in DIRS:
+        if directory in os.listdir(dir):
+            return os.path.join(dir, directory), dir
+
 
 def copy(directory, new_directory):
+    directory_c = directory
+    new_directory_c = new_directory
+    directory, base_dir = search_dir(directory)
+    new_directory = os.path.join(base_dir, new_directory)
     if not os.path.exists(new_directory):
         os.makedirs(new_directory)
     else:
-        raise Exception("Directory already exists")
+        raise BaseException("Directory already exists")
 
     all_files = glob.glob(os.path.join(directory, "*"))
     all_files_without_base = [os.path.basename(path) for path in all_files]
+    # all_files_without_base = [
+    #     os.path.join(base_dir, path) for path in all_files_without_base
+    # ]
     all_files_without_base.sort()
     all_files.sort()
     for path in all_files:
         new_path = os.path.join(new_directory, os.path.basename(path))
-        new_path = new_path.replace(directory, new_directory)
+        # if new_directory in new_path:
+        #     pass
+        # else:
+        new_path = new_path.replace(directory_c, new_directory_c)
         os.system(f"cp -r {path} {new_path}")
         cprint(f"Copying {path} to {new_path}", "green")
 
@@ -24,4 +47,5 @@ def copy(directory, new_directory):
 if __name__ == "__main__":
     directory = sys.argv[1]
     new_directory = sys.argv[2]
+    cprint(f"Copying {directory} to {new_directory}", "red")
     copy(directory, new_directory)
---
+++ Off_Campus/CVs/directory_map.md
@@ -0,0 +1,5 @@
+# Directory Map
+
+- `data_analyst`: For CVs where DL is not required. A small ML is required, and the candidate should be able to understand the data and make some analysis. If DLS is added in the certificates, it does not belong in this folder!
+- `finance`: CVs related to finance.
+- `ML_DL`: A lot of ML and DL is required.
